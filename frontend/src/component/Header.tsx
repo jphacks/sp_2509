dbcc95ccf60d18eeb6253e236c350fd9d0e6cc0a
MasakiToshima <toshima.masaki.akita@gmail.com>
2025-10-16 15:40:57 +0900

    ヘッダーの文字を小さくした

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -5,6 +5,6 @@ type HeaderProps = {
 };
 
 
-export default function Header({headerText}: HeaderProps)  {
-    return (<h1 className='text-3xl md:text-4xl font-extrabold tracking-tight'>{headerText}</h1>)
+export default function Header({ headerText }: HeaderProps) {
+  return (<h2 className="text-xl md:text-2xl font-bold tracking-tight">{headerText}</h2>)
 };
