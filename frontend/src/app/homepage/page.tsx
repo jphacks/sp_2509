c15a4862d7961d661c8f5f81f89b381722e38b90
岩井望 <iy.nozomy@gmail.com>
2025-10-18 13:28:14 +0900

    デバッグ用のコード削除

--- frontend/src/app/homepage/page.tsx
+++ frontend/src/app/homepage/page.tsx
@@ -15,8 +15,6 @@ export default function Home() {
         const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
         const fetchCourses = async (userId: string) => {
-            //alert(userId); // userIdを表示
-            //userId = '23af53791bd9448dbe98a095920266ec'; // テスト用に固定
             try {
                 const res = await fetch(`${API_URL}/users/${userId}/courses`);
                 const data = await res.json();
