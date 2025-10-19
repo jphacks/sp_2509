f3b499dac9d062e29454d41797dc42be98dbca36
豊田 真吾 <toyodasinngo@gmail.com>
2025-10-19 09:44:22 +0900

    ESLintのタブ幅などを設定

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -12,6 +12,14 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      indent: ["error", 2, { SwitchCase: 1 }],
+      "react/jsx-indent": ["error", 2],
+      "react/jsx-indent-props": ["error", 2],
+      "@typescript-eslint/indent": ["error", 2, { SwitchCase: 1 }],
+    },
+  },
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
